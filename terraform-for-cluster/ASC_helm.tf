04a0dcc4a5fe161a8bdf1b9f9ad988fa15cdb429
Ubuntu <rsh4297@gmail.com>
2022-07-08 15:42:42 +0000

    Modif autoscaling-set

--- terraform-for-cluster/ASC_helm.tf
+++ terraform-for-cluster/ASC_helm.tf
@@ -1,39 +1,24 @@
 resource "helm_release" "cluster-autoscaler-controller" {
   name = "cluster-autoscaler-controller"
 
-  repository = "https://kubernetes.github.io/autoscaler"
+  repository = "https://nomer26.github.io/Charts/charts/stable"
   chart = "cluster-autoscaler"
   namespace = "kube-system"
 
-  set {
-    name = "awsRegion"
-    value = local.region
-  }
+  values = [
+    "${file("values.yaml")}"
+  ]
 
   set {
-    name = "replicaCount"
-    value = "2"
-  }
-
-  set { 
-    name = "image.tag"
-    value = "v1.12.0"
-  }
-
-  set {
-    name = "autoDiscovery.clusterName"
+    name = "clusterName"
     value = aws_eks_cluster.eks.id
   }
 
   set {
-    name ="rbac.serviceAccount.name"
-    value = "cluster-autoscaler"
-  }
-
-  set {
-    name = "rbac.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    name = "annotations.rolearn"
     value = aws_iam_role.eks_cluster_autoscaler.arn
   }
+
   depends_on = [
     aws_eks_node_group.nodes_general,
     aws_iam_role_policy_attachment.eks_cluster_autoscaler_attach
